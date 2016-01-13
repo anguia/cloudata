7a629c70e5bf802348dbf09267070293527a171e
wangwx <wangwx@onecloud.cn>
2016-01-13 11:17:30 +0800

    add deploy key

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Configure deploy key"
+cp ./id_rsa.pub /root/.ssh/
+chmod 700 /root/.ssh/id_rsa.pub
+chown -R root:root /root/.ssh
+
+echo "get project"
+git clone http://clouddata.f3322.net:13000/cloud-data/hifood-android.git
