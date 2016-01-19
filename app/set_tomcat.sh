e7104f298a2f055be5d0bbd098cde2b1bf9ca40b
wangwx <wangwx@onecloud.cn>
2016-01-19 18:00:21 +0800

    setup-database.sh

---
+++ app/set_tomcat.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+if [ -f /.tomcat_configured ]; then
+	echo "=> Tomcat has been configured!"
+	exit 0
+fi
+
+echo "=> unzip app War"
+
+rm -rf /usr/local/tomcat/webapps/WlanBIPortal
+unzip /usr/local/tomcat/webapps/WlanBIPortal.war -d /usr/local/tomcat/webapps/WlanBIPortal
+
+echo "=> Configure Tomcat JDBC Connection"
+
+sed -i -e "s/<--POSTGRES_HOST-->/${POSTGRES_HOST}/g" \
+	-e "s/<--POSTGRES_PORT-->/${POSTGRES_PORT}/g" \
+	-e "s/<--POSTGRES_DB_NAME-->/${POSTGRES_DB_NAME}/g" \
+	-e "s/<--POSTGRES_USER-->/${POSTGRES_USER}/g" \
+	-e "s/<--POSTGRES_PASS-->/${POSTGRES_PASS}/g" /usr/local/tomcat/webapps/WlanBIPortal/jdbc.properties
+
+touch /.tomcat_configured
+
+echo "=> Configure Tomcat JDBC Connection as follows:"
+echo "   jdbc.url=jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB_NAME}"
+echo "   jdbc.username=${POSTGRES_USER}"
+echo "   jdbc.password=${POSTGRES_PASS}"
+
+echo "   ** Please check your environment variables if you find somethin is misconfigured.**"
+echo "=> Done!"
