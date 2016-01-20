1de4f7118504e512951940b0d0e959982bd35d2f
wangwx <wangwx@onecloud.cn>
2016-01-20 16:11:25 +0800

    update

--- postgresql/setup-database.sh
+++ postgresql/setup-database.sh
@@ -4,25 +4,21 @@
 #EOSQL`
 TEST=1
 if [[ $TEST == "1" ]]; then
-# database exists
-# $? is 0
-exit 0
+	# database exists
+	# $? is 0
+	exit 0
 else
-echo "******CREATING DOCKER DATABASE******"
-if [[ $TEST == "1" ]]; then
-gosu postgres postgres --single <<- EOSQL
-CREATE ROLE $DB_USER WITH LOGIN ENCRYPTED PASSWORD '${DB_PASS}' CREATEDB;
-EOSQL
-gosu postgres postgres --single <<- EOSQL
-CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0 ENCODING 'UTF8';
-EOSQL
-gosu postgres postgres --single <<- EOSQL
-GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
-EOSQL
-cd /docker-entrypoint-initdb.d/config
-psql -U$DB_USER -d$DB_NAME < install.sql
-cd -
-
+	echo "******CREATING DOCKER DATABASE******"
+	gosu postgres postgres --single <<- EOSQL
+	CREATE ROLE $DB_USER WITH LOGIN ENCRYPTED PASSWORD '${DB_PASS}' CREATEDB;
+	EOSQL
+	gosu postgres postgres --single <<- EOSQL
+	CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0 ENCODING 'UTF8';
+	EOSQL
+	gosu postgres postgres --single <<- EOSQL
+	GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+	EOSQL
+	cd /docker-entrypoint-initdb.d/config
+	psql -U$DB_USER -d$DB_NAME < install.sql
+	cd -
 fi
-echo ""
-echo "******DOCKER DATABASE CREATED******"
